3c7cee27b7b05c5fdfd8d651b3d0f493cdd40ea5
Jimmy Eung <jimmy.eung@student.ecp.fr>
2016-05-19 14:35:51 +0200

    Blabla

---
+++ app/src/main/java/com/example/matthieu/testproject/Product.java
@@ -0,0 +1,21 @@
+package com.example.matthieu.testproject;
+
+/**
+ * Created by eung on 19/05/16.
+ */
+public class Product {
+
+    private int id;
+
+    public Product(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
